bbe0f7da1b0efe56fd91c219d083d9b5dc5fcf90
Neyber Rojas <nrojas@ucb.edu.bo>
2025-06-13 14:12:31 -0400

    fix: add top-level constants to satisfy Zeitwerk autoloading requirements

--- app/domain.rb
+++ app/domain.rb
@@ -3,3 +3,7 @@
 # This file defines the Domain module namespace
 module Domain
 end
+
+# Define a top-level constant to satisfy Zeitwerk autoloading
+# This is necessary because Zeitwerk expects app/domain.rb to define Domain
+Domain = ::Domain
--- app/domain/entities.rb
+++ app/domain/entities.rb
@@ -2,6 +2,11 @@
 
 # This file defines the Entities module namespace within Domain
 module Domain
+  # Entities module contains all domain entities
   module Entities
   end
 end
+
+# Define a top-level constant to satisfy Zeitwerk autoloading
+# This is necessary because Zeitwerk expects app/domain/entities.rb to define Entities
+Entities = Domain::Entities
